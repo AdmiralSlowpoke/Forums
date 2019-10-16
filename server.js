7e073ea3dfa662d11ecceac8faacdb6b7249e874
AdmiralSlowpoke <38550899+AdmiralSlowpoke@users.noreply.github.com>
2019-10-16 16:55:38 +0400

    1

---
+++ server.js
@@ -0,0 +1,21 @@
+let express = require('express');
+let app = express();
+app.use(express.static(__dirname + '/public')); 
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: false })); 
+let msg=[]; // храним сообщения в массиве
+// обработка запросов
+app.get('/addcomment', function(req, res) { // добавить сообщение
+  msg.push(req.query.newmsg);
+  res.send({message:msg.length}); 
+});
+app.get('/lastcomments', function(req, res) { // последние 10 сообщений
+  res.send(msg.slice(-10)); 
+});
+app.get('/testcommand',function(req,res){
+    res.send({error:{message:'Произошла ошибка',code:304}});
+});
+
+app.listen(3000, function () {
+  console.log('Listening on port 3000!');
+});
